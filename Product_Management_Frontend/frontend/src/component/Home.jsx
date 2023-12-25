753308454e1806b2b31df216f0f701f7a7a95e70
dhiraj29patil <dhiraj29patil@gmail.com>
2023-12-25 11:32:30 +0530

    Frontend Commit

--- Product_Management_Frontend/frontend/src/component/Home.jsx
+++ Product_Management_Frontend/frontend/src/component/Home.jsx
@@ -1,40 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductService from "../service/product_service";
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+
+    const [productList, setProductList] = useState([]);
+    const [msg, setmsg] = useState("");
+
+    useEffect(() => {
+        init();
+    }, []);
+
+    const init=()=>{
+        ProductService
+            .getAllProduct()
+            .then((res) => {
+                console.log(res.data);
+                setProductList(res.data);
+            }).catch((error) => {
+                console.log(error);
+            })
+    }
+
+    const deleteProduct = (id) => {
+        ProductService.deleteProduct(id).then((res)=>{
+            setmsg("Delete Successfully");
+            init();
+        }).catch((error)=>{
+            console.log(error);
+        })
+    }
     return (
         <>
             <div className='container mt-3'>
                 <div className="row">
                     <div className="col-md-12">
                         <div className="card">
+                            <div className="card-header fs-3 text-center">
+                                All Product List
+                                {
+                                    msg &&
+                                    <p className='fs-4 text-center text-success'>{msg}</p>
+                                }
+                            </div>
                             <div className="card-body">
                                 <table class="table">
                                     <thead>
-                                        <tr>
-                                            <th scope="col">#</th>
-                                            <th scope="col">First</th>
-                                            <th scope="col">Last</th>
-                                            <th scope="col">Handle</th>
-                                        </tr>
+                                        {
+
+                                            <tr>
+                                                <th scope="col">Sr.No</th>
+                                                <th scope="col">Product Name</th>
+                                                <th scope="col">Description</th>
+                                                <th scope="col">Price</th>
+                                                <th scope="col">Status</th>
+                                                <th scope="col">Action</th>
+                                            </tr>
+
+                                        }
+
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <th scope="row">1</th>
-                                            <td>Mark</td>
-                                            <td>Otto</td>
-                                            <td>@mdo</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">2</th>
-                                            <td>Jacob</td>
-                                            <td>Thornton</td>
-                                            <td>@fat</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">3</th>
-                                            <td colspan="2">Larry the Bird</td>
-                                            <td>@twitter</td>
-                                        </tr>
+                                        {
+                                            productList.map((p, num) => (
+                                                <tr>
+                                                    <td>{num + 1}</td>
+                                                    <td>{p.productName}</td>
+                                                    <td>{p.description}</td>
+                                                    <td>{p.price}</td>
+                                                    <td>{p.status}</td>
+                                                    <td>
+                                                        <Link to="" className='btn btn-sm btn-primary'>Edit</Link>
+                                                        <button onClick={() => deleteProduct(p.id)} className='btn btn-sm btn-danger ms-1'>Delete</button>
+                                                    </td>
+                                                </tr>
+                                            ))
+                                        }
                                     </tbody>
                                 </table>
                             </div>
